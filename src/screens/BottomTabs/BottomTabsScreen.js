00a2bb39dc239acae09cc792af107ccbf043bdaf
Khalil <khalil.daoulat@adria-bt.com>
2022-10-25 06:18:36 +0100

    add home screen

--- src/screens/BottomTabs/BottomTabsScreen.js
+++ src/screens/BottomTabs/BottomTabsScreen.js
@@ -2,20 +2,43 @@ import Home from "../HomeScreen";
 import SendRequest from "../SendRequestScreen";
 import Cards from "../CardsScreen";
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import {MaterialIcons} from "@expo/vector-icons";
 
 export default function BottomTabsScreen() {
     const TabStack = createBottomTabNavigator();
-    const tabBarOptions = {
-        tabBarActiveTintColor: '#0a48a7',
-        tabBarInactiveTintColor: 'black',
-        backgroundColor: "#1e1e1e",
-        paddingBottom : 32
-    }
+
+    const screenOptions = ({route}) => ({
+        tabBarIcon: ({focused}) => {
+            let icon = "";
+            const color = focused ? "#559dff" : "#828282";
+            const size = 24;
+            switch(route.name){
+                case "Cards" :
+                    icon = "credit-card";
+                    break;
+                case "Home" :
+                    icon = "home";
+                    break;
+                case "SendRequest" :
+                    icon = "send";
+                    break;
+                default :
+                    icon = "dashboard";
+            }
+            return <MaterialIcons name={icon} size={size} color={color}/>
+        },
+        tabBarStyle: {
+            backgroundColor: "#1e1e1e",
+            borderTopColor:"#1e1e1e",
+            paddingBottom:32,
+        },
+     });
+
     return (
-        <TabStack.Navigator screenOptions = {() => tabBarOptions}>
-            <TabStack.Screen name="Home" component={Home} />
-            <TabStack.Screen name="Send Request" component={SendRequest} />
-            <TabStack.Screen name="Cards" component={Cards} />
+        <TabStack.Navigator screenOptions = {screenOptions}>
+            <TabStack.Screen name="Home" component={Home} options={{ headerShown: false }}/>
+            <TabStack.Screen name="SendRequest" component={SendRequest} options={{ title : "Send & Request", headerShown: false }}/>
+            <TabStack.Screen name="Cards" component={Cards} options={{ title : "My Cards" , headerShown: false }}/>
         </TabStack.Navigator>
     );
 }
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,18 +1,41 @@
-import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components';
+import Text from '../components/Text';
+import {FontAwesome5} from "@expo/vector-icons";
+
 
 export default function HomeScreen() {
     return (
-        <View style={styles.container}>
-            <Text>Hello Home</Text>
-        </View>
+        <Container>
+            <Header>
+                <ProfilePhoto source={require("../../assets/logo.png")}/>
+                <Welcome>
+                    <Text>Welcome,</Text>
+                    <Text>Design</Text>
+                </Welcome>
+                <FontAwesome5 name="cog" size={24} color="#565656"/>
+            </Header>
+            <StatusBar barStyle="light-content"/>
+        </Container>
     );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
+const Container = styled.SafeAreaView`
+    flex : 1;
+    background-color: #1e1e1e;
+`;
+
+const Header = styled.View`
+    flex-direction : row,
+    align-items : center;
+    margin : 16px 16px 32px 16px;
+`;
+
+const ProfilePhoto = styled.Image`
+    width:60px;
+    height : 60px;
+    border-radius: 20px;
+`;
+
+const Welcome = styled.View``;
+
+const StatusBar = styled.StatusBar``;
