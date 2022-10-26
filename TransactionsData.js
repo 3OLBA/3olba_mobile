cdb848ad73e59f1250d769eb14f0b721af062970
Khalil <khalil.daoulat@adria-bt.com>
2022-10-26 09:21:42 +0100

    add Transactions view

---
+++ TransactionsData.js
@@ -0,0 +1,47 @@
+export default [
+    {
+        id : "2j2v370dbdosmslsj",
+        product : "Computer",
+        store : "Marjanne",
+        price : "5,789.00",
+        currency : "MAD",
+        date : "22-10-2022",
+        Address : "Menara Mall Casablanca"
+    },
+    {
+        id : "2j2v370dbdosmslsj",
+        product : "Computer",
+        store : "Carrefour",
+        price : "4,300.00",
+        currency : "MAD",
+        date : "22-10-2022",
+        Address : "Menara Mall Casablanca"
+    },
+    {
+        id : "6gdfb6565545454",
+        product : "Computer",
+        store : "Micro Planet",
+        price : "1,789.00",
+        currency : "MAD",
+        date : "22-10-2022",
+        Address : "Menara Mall Casablanca"
+    },
+    {
+        id : "1jnf7404hfnr4",
+        product : "Computer",
+        store : "Marjanne",
+        price : "5,789.00",
+        currency : "MAD",
+        date : "22-10-2022",
+        Address : "Menara Mall Casablanca"
+    },
+    {
+        id : "9789f442hn3g3",
+        product : "Computer",
+        store : "Marjanne",
+        price : "5,789.00",
+        currency : "MAD",
+        date : "22-10-2022",
+        Address : "Menara Mall Casablanca"
+    }
+]
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,19 +1,56 @@
 import styled from 'styled-components';
 import Text from '../components/Text';
-import {FontAwesome5} from "@expo/vector-icons";
+import {FontAwesome5,MaterialIcons , AntDesign} from "@expo/vector-icons";
 
+import TransactionData from "../../TransactionsData";
 
 export default function HomeScreen() {
+    const renderTransactions = ({item}) => {
+        return (
+                <Purshase>
+                    <PurshaseInfo>
+                        <Text>{item.product}</Text>
+                        <Text>{item.store}</Text>
+                        <Text>{item.address}</Text>
+                    </PurshaseInfo>
+                    <Text>{item.price}</Text>
+                </Purshase>
+            )
+    }
     return (
         <Container>
             <Header>
                 <ProfilePhoto source={require("../../assets/logo.png")}/>
                 <Welcome>
-                    <Text>Welcome,</Text>
-                    <Text>Design</Text>
+                    <Text heavy medium>Welcome,</Text>
+                    <Text>Khalil</Text>
                 </Welcome>
                 <FontAwesome5 name="cog" size={24} color="#565656"/>
             </Header>
+
+            <Text center title black>
+                7,500.00 dh
+            </Text>
+            <Text center medium color="#727479">
+                Current balance
+            </Text>
+
+            <Purshases ListHeaderComponent={
+                <>
+                    <TransactionsHeader>
+                        <Text>Last Transactions</Text>
+                        <MaterialIcons name="sort" size={24} color="#5196f4" />
+                    </TransactionsHeader>
+                    <SearchContainer>
+                        <AntDesign name="search1" size={18} color="#5196f4" />
+                        <Search placeHolder="Search transaction" />
+                    </SearchContainer>
+                </>
+            }
+                       data={TransactionData} renderItem={renderTransactions} showVerticalScrollIndicator={false}
+            />
+
+
             <StatusBar barStyle="light-content"/>
         </Container>
     );
@@ -25,17 +62,53 @@ const Container = styled.SafeAreaView`
 `;
 
 const Header = styled.View`
-    flex-direction : row,
+    flex-direction : row;
     align-items : center;
-    margin : 16px 16px 32px 16px;
+    margin : 16px 16px 32px 16px; 
 `;
 
 const ProfilePhoto = styled.Image`
-    width:60px;
+    width : 60px;
     height : 60px;
-    border-radius: 20px;
+    border-radius : 60px;
 `;
 
-const Welcome = styled.View``;
+const Welcome = styled.View`
+    flex : 1;
+    padding : 0 16px ;
+`;
+
+const Purshases = styled.FlatList`
+    background-color : #2c2c2c;
+    padding : 16px;
+`;
+
+const Purshase = styled.View`
+`;
+
+const PurshaseInfo = styled.View`
+`;
+
+const TransactionsHeader = styled.View`
+    flex-direction : row;
+    align-items : center;
+    justify-content : space-between;
+`;
+
+const SearchContainer = styled.View`
+    background-color : #3d3d3d;
+    flex-direction : row;
+    align-items : center;
+    padding : 0 8px;
+    border-radius : 6px;
+    margin : 16px 0px;
+`;
+
+const Search = styled.TextInput`
+    flex : 1;
+    padding : 8px 16px;
+    font-family : "Avenir";
+    color : #dbdbdb
+`;
 
 const StatusBar = styled.StatusBar``;
